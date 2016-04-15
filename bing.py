7abc93ef6719512ae86fab731496035abe82ee57
Nathaniel Olsen <NateTenn5123@Gmail.com>
2016-04-14 23:39:57 -0500

    Update bing.py

--- bing.py
+++ bing.py
@@ -13,9 +13,15 @@ def main():
 		prefix2 = random.choice(pornlist['prefix2'])
 		suffix = random.choice(pornlist['suffix'])
 		suffix2 = random.choice(pornlist['suffix2'])
-		final = prefix + prefix2 + searchfor + suffix + suffix2
-		print("Did you mean: %s (Opening in 3 seconds)" % (final))
-		time.sleep(3)
-		webbrowser.open('https://bing.com/search?q=%s' % (final))
+		if len(prefix) != 0:
+			final = prefix + prefix2 + searchfor + suffix + suffix2
+			print("Did you mean: %s (Opening in 3 seconds)" % (final))
+			time.sleep(3)
+			webbrowser.open('https://bing.com/search?q=%s' % (final))
+		else:
+			final = searchfor + prefix2 + suffix + suffix2
+			print("Did you mean: %s (Opening in 3 seconds)" % (final))
+			time.sleep(3)
+			webbrowser.open('https://bing.com/search?q=%s' % (final))
 
 main()
