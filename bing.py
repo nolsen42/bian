6a6000b5303dddb9f2ac1c75a853ae178d9aefb4
Nathaniel Olsen <NateTenn5123@Gmail.com>
2016-04-14 23:22:23 -0500

    Update bing.py

--- bing.py
+++ bing.py
@@ -1,10 +1,21 @@
 import webbrowser
+import json
+import random
+import time
+
+with open("pornlist.json") as f:
+    pornlist = json.load(f)
 
 def main():
 	while True:
 		searchfor = input("Search (with Bing): ")
-		print("Did you mean %s porn?" % (searchfor))
-		print('https://bing.com/search?q=%s+porn (Opening that for you)' % (searchfor))
-		webbrowser.open('https://bing.com/search?q=%s+porn' % (searchfor))
+		prefix = random.choice(pornlist['prefix'])
+		prefix2 = random.choice(pornlist['prefix2'])
+		suffix = random.choice(pornlist['suffix'])
+		suffix2 = random.choice(pornlist['suffix2'])
+		final = prefix + prefix2 + searchfor + suffix + suffix2
+		print("Did you mean: %s (Opening in 3 seconds)" % (final))
+		time.sleep(3)
+		webbrowser.open('https://bing.com/search?q=%s' % (final))
 
 main()
